8d3d14f4bc68e9c5b80ff96f170d6877d25c23f5
Jagermaestro1 <Jonas@Jonass-MacBook-Air.local>
2016-03-16 13:44:58 +0100

    dsfghjkl

---
+++ Skånetrafiken/src/skaneAPI/view/TestClass.java
@@ -0,0 +1,34 @@
+package se.mah.k3lara.skaneAPI.view;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import se.mah.k3lara.skaneAPI.control.Constants;
+import se.mah.k3lara.skaneAPI.model.Journey;
+import se.mah.k3lara.skaneAPI.model.Journeys;
+import se.mah.k3lara.skaneAPI.model.Line;
+import se.mah.k3lara.skaneAPI.model.Lines;
+import se.mah.k3lara.skaneAPI.model.Station;
+import se.mah.k3lara.skaneAPI.xmlparser.Parser;
+
+public class TestClass {
+
+	public static void main(String[] args) { 
+
+		// från malmö c - lund c med 2st sökresultat
+		String searchURL = Constants.getURL("80002","80810",2); 
+		System.out.println(searchURL);
+		System.out.println("// Results when searching:");
+		
+		//hämtar resor från skånetrafiken
+		Journeys journeys = Parser.getJourneys(searchURL);
+		for (Journey journey : journeys.getJourneys()) {
+			System.out.print(journey.getStartStation()+" - ");
+			System.out.print(journey.getEndStation());
+			String time = journey.getDepDateTime().get(Calendar.HOUR_OF_DAY)+":"+journey.getDepDateTime().get(Calendar.MINUTE);
+			System.out.println(" Departs in "+journey.realDepTime() +" motherfuckingminutes");
+		} 
+		
+
+	}
+}
