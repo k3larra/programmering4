f5066486acde23b22cef4fc154548decae33b9ec
antonilund <antonibagis@gmail.com>
2016-03-22 14:45:29 +0100

    SkaneScreen 1.4

--- Grupparbete/src/se/mah/k3/grupp4/Gui2.java
+++ Grupparbete/src/se/mah/k3/grupp4/Gui2.java
@@ -67,9 +67,9 @@ public class Gui2 extends JFrame {
 		setBackground(Color.GRAY);
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		double width = 1920;// screenSize.getWidth();
-		double height = 1080;// screenSize.getHeight();
-
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		int labelWidth = 120;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBounds(0, 0, (int) width, (int) height);
 		this.setUndecorated(true);
@@ -80,118 +80,118 @@ public class Gui2 extends JFrame {
 		
 		label = new JLabel("");
 		label.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label.setBounds(640, 361, 117, 51);
+		label.setBounds(640, 361, labelWidth, 51);
 		contentPane.add(label);
 
 		label_1 = new JLabel("");
 		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_1.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_1.setBounds(767, 361, 100, 51);
+		label_1.setBounds(767, 361, labelWidth, 51);
 		contentPane.add(label_1);
 
 		label_2 = new JLabel("");
 		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_2.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_2.setBounds(767, 479, 100, 51);
+		label_2.setBounds(767, 479, labelWidth, 51);
 		contentPane.add(label_2);
 
 		label_3 = new JLabel("");
 		label_3.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_3.setBounds(640, 479, 117, 51);
+		label_3.setBounds(640, 479, labelWidth, 51);
 		contentPane.add(label_3);
 
 		label_4 = new JLabel("");
 		label_4.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_4.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_4.setBounds(767, 602, 100, 51);
+		label_4.setBounds(767, 602, labelWidth, 51);
 		contentPane.add(label_4);
 
 		label_5 = new JLabel("");
 		label_5.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_5.setBounds(640, 602, 117, 51);
+		label_5.setBounds(640, 602, labelWidth, 51);
 		contentPane.add(label_5);
 
 		label_6 = new JLabel("");
 		label_6.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_6.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_6.setBounds(767, 722, 100, 51);
+		label_6.setBounds(767, 722, labelWidth, 51);
 		contentPane.add(label_6);
 
 		label_7 = new JLabel("");
 		label_7.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_7.setBounds(640, 722, 117, 51);
+		label_7.setBounds(640, 722, labelWidth, 51);
 		contentPane.add(label_7);
 
 		label_8 = new JLabel("");
 		label_8.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_8.setFont(new Font("Gill Sans MT", Font.PLAIN, 30));
-		label_8.setBounds(777, 846, 100, 51);
+		label_8.setBounds(777, 846, labelWidth, 51);
 		contentPane.add(label_8);
 
 		label_9 = new JLabel("");
-		label_9.setFont(new Font("Gill Sans MT", Font.PLAIN, 30));
-		label_9.setBounds(640, 846, 117, 51);
+		label_9.setFont(new Font("Gill Sans MT", Font.BOLD, 30));
+		label_9.setBounds(640, 846, labelWidth, 51);
 		contentPane.add(label_9);
 
 		label_Clock = new JLabel("");
 		label_Clock.setForeground(Color.WHITE);
 		label_Clock.setFont(new Font("Gill Sans MT", Font.PLAIN, 48));
-		label_Clock.setBounds(1621, 43, 165, 117);
+		label_Clock.setBounds(1621, 43, 200, 117);
 		contentPane.add(label_Clock);
 
 		label_11 = new JLabel("");
 		label_11.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_11.setBounds(1542, 361, 100, 51);
+		label_11.setBounds(1542, 361, labelWidth, 51);
 		contentPane.add(label_11);
 
 		label_12 = new JLabel("");
 		label_12.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_12.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_12.setBounds(1668, 361, 100, 51);
+		label_12.setBounds(1668, 361, labelWidth, 51);
 		contentPane.add(label_12);
 
 		label_13 = new JLabel("");
 		label_13.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_13.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_13.setBounds(1669, 479, 100, 51);
+		label_13.setBounds(1669, 479, labelWidth, 51);
 		contentPane.add(label_13);
 
 		label_14 = new JLabel("");
 		label_14.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_14.setBounds(1542, 479, 100, 51);
+		label_14.setBounds(1542, 479, labelWidth, 51);
 		contentPane.add(label_14);
 
 		label_15 = new JLabel("");
 		label_15.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_15.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_15.setBounds(1669, 602, 100, 51);
+		label_15.setBounds(1669, 602, labelWidth, 51);
 		contentPane.add(label_15);
 
 		label_16 = new JLabel("");
 		label_16.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_16.setBounds(1542, 602, 100, 51);
+		label_16.setBounds(1542, 602, labelWidth, 51);
 		contentPane.add(label_16);
 
 		label_17 = new JLabel("");
 		label_17.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_17.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_17.setBounds(1669, 722, 100, 51);
+		label_17.setBounds(1669, 722, labelWidth, 51);
 		contentPane.add(label_17);
 
 		label_18 = new JLabel("");
 		label_18.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_18.setBounds(1542, 722, 100, 51);
+		label_18.setBounds(1542, 722, labelWidth, 51);
 		contentPane.add(label_18);
 
 		label_19 = new JLabel("");
 		label_19.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_19.setFont(new Font("Rockwell", Font.PLAIN, 30));
-		label_19.setBounds(1669, 846, 100, 51);
+		label_19.setBounds(1669, 846, labelWidth, 51);
 		contentPane.add(label_19);
 
 		label_20 = new JLabel("");
 		label_20.setFont(new Font("Rockwell", Font.BOLD, 30));
-		label_20.setBounds(1542, 846, 100, 51);
+		label_20.setBounds(1542, 846, labelWidth, 51);
 		contentPane.add(label_20);
 		
 		scrollPane = new JScrollPane();
